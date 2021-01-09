eb13a87ec8bfd16fa367adcb756f5a2de025ad00
186526 <i@186526.xyz>
2021-01-09 19:35:50 +0800

    Update Publish

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@ git init
 git add .
 git commit -m "new Version"
 git branch -m main
-git remote add origin https://github.com/186526/renexmoe-cdn.git
+git remote add origin git@github.com:186526/renexmoe-cdn.git
 git push -u origin main -f
