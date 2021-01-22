6b0b2766aa5e95b46d46b2c3babff01848796c3d
186526 <i@186526.xyz>
2021-01-22 14:57:53 +0800

    v1.1.4.1-lts Update

--- src/js/lib/cookies.js
+++ src/js/lib/cookies.js
@@ -42,7 +42,8 @@ export default {
       switch (vEnd.constructor) {
         case Number:
           sExpires =
-            vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
+            vEnd === Infinity
+              ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
               : "; max-age=" + vEnd;
           break;
         case String:
